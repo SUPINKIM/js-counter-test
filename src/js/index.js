b321736150b055d7458f74509100ec73643528d4
supin <qorgkr26@sookmyung.ac.kr>
2022-03-13 12:13:17 +0900

    feat: http-server & cypress 설치

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/*const body = document.body;
+
+const addBtn = document.getElementById('add');
+const countTag = document.getElementById('value');
+
+const setCount = (value) => ({ count :  value });
+
+addBtn.addEventListener('click', () => {
+    const newValues = setCount(+countTag.innerText + 1);
+    countTag.innerText = newValues.count;
+});*/
